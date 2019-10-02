d0aa5d08f2012f285701f131ea43c40925d6303f
eunbi go <geunbi38@gmail.com>
2019-10-02 19:11:57 +0900

    drill 04 완성
    2018182002 고은비
    https://www.youtube.com/watch?v=lBuShiulGCc

---
+++ Drill04/character_runs.py
@@ -0,0 +1,40 @@
+from pico2d import *
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+x = 0
+frame = 0
+
+def go_left():
+    x = 795
+    frame = 0
+    while x > 0:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x -= 5
+        delay(0.0001)
+        get_events()
+
+def go_right():
+    x = 0
+    frame = 0
+    while x < 800:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x += 5
+        delay(0.0001)
+        get_events()
+
+while True:
+    go_right()
+    go_left()
+
+close_canvas()
+
