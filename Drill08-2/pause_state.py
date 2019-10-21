07ea946e9ccbb2be6680d97f50e82051f0592ae5
eunbi go <geunbi38@gmail.com>
2019-10-21 19:49:57 +0900

    drill08-2 완성
    2018182002 고은비
    https://www.youtube.com/watch?v=dZp_AHiJAS0
    커밋횟수 26회

---
+++ Drill08-2/pause_state.py
@@ -0,0 +1,59 @@
+from pico2d import *
+import game_framework
+import title_state
+import main_state
+
+name = "PauseState"
+
+image = None
+
+def handle_events():
+    pass
+
+
+def pause(): pass
+
+
+def resume(): pass
+
+def enter():
+    global image
+    image = load_image('pause.png')
+    pass
+
+def exit():
+    global image
+    del(image)
+    pass
+
+def update():
+    pass
+
+time = 0
+
+def draw():
+    global time
+    clear_canvas()
+    main_state.draw()
+    time = time + 1
+    main_state.draw()
+    if time % 3 == 0:
+        image.draw(400, 300, 400, 300)
+    update_canvas()
+    pass
+
+def resume():
+    pass
+
+def handle_events():
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            stop = True
+            game_framework.pop_state()
+    pass
+
+
